039d43c8c26c1f3b40a6dcbd8afd20e77d809601
MadhuraRohinkar <madhura.rohinkar@gmail.com>
2025-08-09 14:56:29 +0530

    backend base url changes

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,5 +1,5 @@
 export const variables = {
-  BACKEND_URL: "http://localhost:4000/api/v1", // replace with your actual backend URL
+  BACKEND_URL: "https://netflix-backend-6qyb.onrender.com/api/v1", // replace with your actual backend URL
   SMALL_IMG_BASE_URL:"https://image.tmdb.org/t/p/w500",
   MOVIE_CATEGORIES:["now_playing","top_rated","popular","upcoming"],
   TV_CATEGORIES:["airing_today","on_the_air","popular","top_rated"]
